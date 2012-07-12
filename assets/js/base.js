7ab8e5e5f3137f755edbeb0ba7ce9f8bfec375c7
ThePaavero <nospam@astudios.org>
2012-07-12 23:25:02 +0300

    Added stuff

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,15 +1,33 @@
 /**
  * Base JS
  *
+ * Consists of some helper functions + the main onload initializations
+ *
  * @author Pekka S.
  * @package JS Game Framework
  */
 
+// Helper functions below
+
+/**
+ * Log to console (pretty much just a shortcut for console.log())
+ *
+ * @param  {string} msg Message to log
+ * @return void
+ */
 function log(msg)
 {
 	console.log(msg);
 }
 
+/**
+ * Include a JS file.
+ * Provides a callback possibility.
+ *
+ * @param  {array}   paths    Paths of JS files (root is set to /assets/js/)
+ * @param  {function} callback Function to call when all files have loaded
+ * @return void
+ */
 function include(paths, callback)
 {
 	var root = _root;
@@ -43,12 +61,35 @@ function include(paths, callback)
 			}
 		};
 	}
-
 }
 
+// Initialization below
+
+/**
+ * On document ready, start the whole show
+ *
+ * @return void
+ */
 $(function()
 {
-	include(['index.js']);
+	// Include all common framework/boilerplate functionalities
+	include(['index.js'], function()
+	{
+		// Then, when that's loaded, include the specific game JS
+		include(['game/game.js'], function()
+		{
+			// Check that it has a "start" method
+			if(typeof game.start !== 'function')
+			{
+				// Sucks, bail
+				throw new Error('You need to create a start method for the game object!');
+				return;
+			}
+
+			// Let the custom game logic take over!
+			game.start();
+		});
+	});
 });
 
 // EOF
---
+++ assets/js/game/game.js
@@ -0,0 +1,12 @@
+/**
+ * Game JS
+ *
+ * Rememeber to use the "game" namespace/object!
+ */
+
+// game.start = function()
+// {
+// 	log('Game has started');
+// }
+
+// EOF
